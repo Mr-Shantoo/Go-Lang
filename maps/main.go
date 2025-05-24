8185bf0319da92058e3db66e256dc5651bed8c63
Mr-Shantoo <maineuddin012@gmail.com>
2025-05-24 22:05:31 +0600

    added maps

---
+++ maps/main.go
@@ -0,0 +1,25 @@
+package main 
+import "fmt"
+func main(){
+	carinventory:=make(map[string]int) //string is key type, int value type
+	carinventory["sedan"]=25
+	carinventory["suv"]=15
+	carinventory["gtr"]=20
+	fmt.Println("car inventory:",carinventory)
+	//directly intialization
+	players:=map[string]int{
+		"messi":1,
+		"ronaldo":2,
+		"maradona":3,
+	}
+	fmt.Println(players)
+	delete(players,"ronaldo")
+	fmt.Println("after deleting:", players)
+	// check if a key is present in a map.. 
+	players["neymar"]=4
+	//go maps not only returns key value bt also returns a boolean.. which indicates if a key exists in the map or not..
+	if goat,ok:=players["messi"];ok{
+		fmt.Println("messi is number:",goat)
+	}
+	//these variables can't be used outside of the if condition..
+}
