3bd22fc12a3fbde205d955a4f050ea9c0264e784
Mr-Shantoo <maineuddin012@gmail.com>
2025-09-30 22:44:55 +0600

    Update Strings.go with improvements

--- data-types-variables/Strings.go
+++ data-types-variables/Strings.go
@@ -3,20 +3,38 @@ package main
 import (
 	"fmt"
 	"strings"
+	
 )
 
 func main() {
-	str := " Hello World"
-	W_index := strings.Index(str, "W")
+	s:="Hello World"
+	fmt.Println(len(s))
+	W_index:=strings.Index(s,"W")
 	fmt.Println(W_index)
-	fmt.Println(str[W_index:])
-	fmt.Println(str[:W_index])
-	str2 := "apple"
-	fmt.Println(strings.Replace(str2, "a", "b", 1))
-	str3 := "go programming"
-	fmt.Println(strings.ToUpper(str3))
-	str4 := strings.ToUpper(str3)
-	fmt.Println(strings.ToLower(str4))
-	fmt.Println(strings.Contains(str3, "go"))
-	fmt.Println(strings.Contains(str3, "golang"))
+	substr:=s[:4]
+	fmt.Println(substr)
+	substr2:=s[2:]
+	fmt.Println(substr2)
+	string1:="go programming"
+	fmt.Println(strings.Replace(string1,"go","went",1))
+
+	fmt.Println(strings.ToLower(string1))
+	fmt.Println(strings.ToUpper(string1))
+
+	str:="University"
+	fmt.Println(strings.HasPrefix(str,"Uni"))
+	fmt.Println(strings.HasSuffix(str,"ty"))
+
+	str2:="A,B,C"
+	parts:=strings.Split(str2,",")
+	fmt.Println(parts)
+	fmt.Println(strings.Join(parts,"-"))
+	s1:="ha"
+	fmt.Println(strings.Repeat(s1,3))
+
+
+
+
+
+	
 }
