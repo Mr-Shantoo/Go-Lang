d1685c5d8cf717f3f64cf2654017e0becc893bdc
Mr-Shantoo <maineuddin012@gmail.com>
2025-10-01 18:12:49 +0600

    updated slices

--- Slices/main.go
+++ Slices/main.go
@@ -19,5 +19,29 @@ GOAT:=players[:3]
 fmt.Println(GOAT)
 newgeneration:=players[len(GOAT):]
 fmt.Println(newgeneration)
+//2D slice 
+rows,cols:=3,2
+grid:=make([][] int,rows)
+for i:=0;i<rows;i++{
+	grid[i]=make([]int, cols)
+	cols++
+}
+fmt.Println(grid)
+count:=1
+for i:=0;i<rows;i++{
+	for j:=0;j<len(grid[i]);j++{
+		grid[i][j]=count
+		count++
+	}
+
+}
+for i:=0;i<rows;i++{
+	for j:=0;j<len(grid[i]);j++{
+		fmt.Printf("%v ",grid[i][j])
+		
+	}
+	fmt.Println("\n")
+
+}
 
 }
