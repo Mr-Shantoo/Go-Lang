7080a2f1f3c80e116976321109b2165436e33933
Mr-Shantoo <maineuddin012@gmail.com>
2025-10-01 17:41:47 +0600

    updated conditional operator

--- conditional_operators/main.go
+++ conditional_operators/main.go
@@ -12,4 +12,10 @@ func main(){
 	}else if !isCitizen{
 		fmt.Println("you are not a citizen")
 	}
+	var isweekend bool=true
+	var isholiday bool=false
+	isdayoff:=isweekend||isholiday
+	if isdayoff{
+		fmt.Println("it's a offday")
+	}
 }
